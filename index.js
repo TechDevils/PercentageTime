4c4779b6ce045e5c5551729a4b6787956c8fba5e
Andrew Chadwick <chadders13@googlemail.com>
2020-08-21 22:46:35 +0100

    Updated total percentages

--- index.js
+++ index.js
@@ -7,29 +7,49 @@ function getPeratageTimeToComplete32Bit(){
     return getPercentage(timeNow, _32BitInt);
 }
 
-function getPerctageOfCurrentMin(){
-    var timeNow = new Date();
+function getPerctageOfCurrentMin(timeNow){
     return getPercentage(timeNow.getUTCSeconds(), _secAndMinConst);
 }
-function getPerctageOfCurrentHour(){
-    var timeNow = new Date();
-    return getPercentage(timeNow.getUTCMinutes(), _secAndMinConst) + (getPerctageOfCurrentMin()/100);
+function getPerctageOfCurrentHour(timeNow, includeSubElement){
+    var addTime = 0;
+
+    if(includeSubElement){
+        addTime = (getPerctageOfCurrentMin(timeNow)/100);
+    }
+
+    return getPercentage(timeNow.getUTCMinutes(), _secAndMinConst) + addTime;
 }
-function getPerctageOfCurrentDay(){
-    var timeNow = new Date();
-    return getPercentage(timeNow.getUTCHours(), _hourConst) + (getPerctageOfCurrentHour() / 100);
+function getPerctageOfCurrentHourMin(timeNow){
+    return getPerctageOfCurrentHour(timeNow, true);
 }
-function getPerctageOfCurrentYear(){
-    var timeNow = new Date();
+function getPerctageOfCurrentDay(timeNow, includeSubElement){
+    var addTime = 0;
+
+    if(includeSubElement){
+        addTime = (getPerctageOfCurrentHourMin(timeNow) / 100);
+    }
+    return getPercentage(timeNow.getUTCHours(), _hourConst) + addTime;
+}
+function getPerctageOfCurrentDayHourMin(timeNow){
+    return getPerctageOfCurrentDay(timeNow,true);
+}
+function getPerctageOfCurrentYear(timeNow, includeSubElement){
     var currentYear = new Date(timeNow.getUTCFullYear(),0,1);
     var numberOfDays = Math.ceil((timeNow.getTime() - currentYear + 1) / 86400000);
     var daysInTheYear = 365;
     if(timeNow.getUTCFullYear() % 4 == 0){
         daysInTheYear++;
     }
+    var addTime = 0;
 
-    return getPercentage(numberOfDays,daysInTheYear) + (getPerctageOfCurrentDay() / 100);
+    if(includeSubElement){
+        addTime = ((getPerctageOfCurrentDayHourMin(timeNow) / 100)/daysInTheYear);
+    }
+    return getPercentage(numberOfDays,daysInTheYear) + addTime;
+}
+function getPerctageOfCurrentYearDayHourMin(timeNow){
 
+    return getPerctageOfCurrentYear(timeNow, true);
 }
 function getPerctageOfCurrentDecade(){
 
@@ -42,8 +62,12 @@ function getPercentage(dividend, divisor){
     return ((dividend / divisor) * 100);
 }
 
+var d = new Date();
 console.log(`perctageTo32BitInt: ${getPeratageTimeToComplete32Bit()}%`);
-console.log(`getPerctageOfCurrentMin: ${getPerctageOfCurrentMin()}%`);
-console.log(`getPerctageOfCurrentHour: ${getPerctageOfCurrentHour()}%`);
-console.log(`getPerctageOfCurrentDay: ${getPerctageOfCurrentDay()}%`);
-console.log(`getPerctageOfCurrentYear: ${getPerctageOfCurrentYear()}%`);
+console.log(`getPerctageOfCurrentMin: ${getPerctageOfCurrentMin(d)}%`);
+console.log(`getPerctageOfCurrentHour: ${getPerctageOfCurrentHour(d)}%`);
+console.log(`getPerctageOfCurrentHour: ${getPerctageOfCurrentHourMin(d)}%`);
+console.log(`getPerctageOfCurrentDay: ${getPerctageOfCurrentDay(d)}%`);
+console.log(`getPerctageOfCurrentDay: ${getPerctageOfCurrentDayHourMin(d)}%`);
+console.log(`getPerctageOfCurrentYear: ${getPerctageOfCurrentYear(d)}%`);
+console.log(`getPerctageOfCurrentYear: ${getPerctageOfCurrentYearDayHourMin(d)}%`);
